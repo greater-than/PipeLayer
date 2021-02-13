deb33d4921a9aaa0a4122e0556c24ffda9b67500
Andrew Benson <andrew.e.benson@gmail.com>
2021-02-13 09:47:58 -0700

    * removes unnecessary getter/setters

--- src/pipelayer/event_args.py
+++ src/pipelayer/event_args.py
@@ -10,35 +10,11 @@ class FilterEventArgs:
     def __init__(
         self, data: Any, context: Context, state: State
     ) -> None:
-        self.__data = data
+        self.data: Any = data
         self.__context = context
-        self.__state = state
-        self.__action = Action.CONTINUE
-
-    @property
-    def data(self) -> Any:
-        return self.__data
-
-    @data.setter
-    def data(self, value: Any) -> None:
-        self.__data = value
+        self.state: State = state
+        self.action: Action = Action.CONTINUE
 
     @property
     def context(self) -> Context:
         return self.__context
-
-    @property
-    def action(self) -> Any:
-        return self.__action
-
-    @action.setter
-    def action(self, value: Action) -> None:
-        self.__action = value
-
-    @property
-    def state(self) -> State:
-        return self.__state
-
-    @state.setter
-    def state(self, value: State) -> None:
-        self.__state == value
